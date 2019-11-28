1a8f8b88d3455007c61207df65e9e083336b2df9
Rainbow-Code <40020051+Rainbow-Code@users.noreply.github.com>
2019-11-28 22:54:18 +0800

    add static files/register and signin is work

--- www/app.py
+++ www/app.py
@@ -1,9 +1,10 @@
 import logging;logging.basicConfig(level=logging.INFO)
 import asyncio,os,json,time
+import orm,config
 from datetime import datetime
 from aiohttp import web
 from jinja2 import Environment,FileSystemLoader
-import orm,config
+from handlers import cookie2user,COOKIE_NAME
 from coroweb import add_routes,add_static
 
 def init_jinja2(app,**kw):
@@ -33,6 +34,21 @@ async def logger_factory(app,handler):
 		#await asyncio.sleep(0.3)
 		return (await handler(request))
 	return logger
+
+async def auth_factory(app,handler):
+	async def auth(request):
+		logging.info('check user: %s %s' % (request.method,request.path))
+		request.__user__ = None
+		cookie_str = request.cookies.get(COOKIE_NAME)
+		if cookie_str:
+			user = await cookie2user(cookie_str)
+			if user:
+				logging.info('set current user: %s' % user.email)
+				request.__user__ = user
+		if request.path.startswith('/manage/') and (request.__user__ is None or not request.__user__.admin):
+			return web.HTTPFound('/signin')
+		return (await handler(request))
+	return auth
 
 async def data_factory(app,handler):
 	async def parse_data(request):
@@ -65,6 +81,7 @@ async def response_factory(app,handler):
 		if isinstance(r,dict):
 			template = r.get('__template__')
 			if template is None:
+				#去IO编程/序列化这一章看json.dumps的介绍
 				resp = web.Response(body=json.dumps(r,ensure_ascii=False,default=lambda o:o.__dict__).encode('utf-8'))
 				resp.content_type = 'application/json;charset=utf-8'
 				return resp
@@ -98,9 +115,9 @@ def datetime_filter(t):
 	return u'%s年%s月%s日' %(dt.year,dt.month,dt.day)
 
 async def init():
-	await orm.create_pool(loop=loop,**config.getConfigs().db)
+	await orm.create_pool(loop=loop,**config.configs.db)
 	app = web.Application(middlewares=[
-			logger_factory,response_factory
+			logger_factory,auth_factory,response_factory
 		])
 	init_jinja2(app,filters=dict(datetime=datetime_filter))
 	add_routes(app,'handlers')
